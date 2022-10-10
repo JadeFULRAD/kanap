b37d8826dff93de504694ad1f2d231ed01f3b7a3
JADE <jade.fulrad@live.fr>
2022-10-10 09:46:33 +0200

    Affichage des elements dans le DOM

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,31 @@
 // Récuperer les datas au format JSON dans le back (Product.js) en faisant une requête via la méthode Fetch...
-
+const listeProduit = document.getElementById("items");
 const fetchProduitApi = () => {
 	fetch("http://localhost:3000/api/products")
-		.then((reponse) => reponse.json()) // ici une Promise nous sera retourné
+		.then((reponse) => reponse.json()) // ici une Promise nous sera retourné au format JSON
 		.then((data) => {
+			// la Promise sera retourné si résolue
 			console.log("data", data);
+			productsDisplay(data);
 		})
-		.catch((err) => console.log(err)); // ici cette fonction s'exécutera si une erreur est survenue
+		.catch((err) => console.log(err)); // Sinon cette fonction s'exécutera si une erreur est survenue
 };
 fetchProduitApi();
+
+// Afficher les données récupérées et les afficher via le DOM
+const productsDisplay = (products) => {
+	for (let i in products) {
+		// On créé une boucle for afin de parcourir le tableau
+		console.log(products[i]);
+
+		// On créé les élements à afficher
+		/*let itemLink = document.createElement("a");*/
+		/*itemLink.setAttribute("href", products.html);
+	itemLink.appendChild(listeProduit);*/
+		listeProduit.innerHTML += `<a href="#">
+		<article>
+		<img src="${products[i].imageUrl}">
+		</article>
+		</a>`;
+	}
+};
